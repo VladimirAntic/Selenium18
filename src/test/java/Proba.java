3daf51ebc5e520e92416545e30aec75ddb35bb86
Your Name <you@example.com>
2024-01-21 23:50:53 +0100

    demoqa.com

--- src/test/java/Proba.java
+++ src/test/java/Proba.java
@@ -7,14 +7,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class Proba {
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.manage().window().maximize();
 
